e7190095fe7b7d3aab17d4264fb69e449b1a8c62
Luana Copetti <luana.copetti@hotmail.com>
2023-04-11 14:21:46 -0300

    classe produto adicionada

--- Produto.cs
+++ Produto.cs
@@ -1,8 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace GerenciamentoVendasLojaRoupas
 {
-	public Class1()
-	{
-	}
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Preco { get; set; }
+        public string Categoria { get; set; }
+
+    }
+    public Produto(string nome, string Descricao, string Preco, string Categoria)
+    {
+        Nome = nome
+        Descricao = descricao;
+        Preco = preco;
+        Categoria = categoria;
+    }
 }
+
