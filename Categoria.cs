e31f2ca9bf47403d511d8d86edc77e67e6e14d90
Luana Copetti <luana.copetti@hotmail.com>
2023-04-11 14:16:28 -0300

    adicionei a classe categoria

---
+++ Categoria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGerenciamentoVendasLojaRoupas
+{
+    internal class Categoria
+    {
+        public string Nome { get; set; }
+        public Categoria(string nome)
+        {
+            Nome = nome;
+        }
+    }
+    public class Loja
+    {
+        public List<Categoria> CategoriasDisponiveis { get; set; }
+        public Loja()
+        {
+            CategoriasDisponiveis = new List<Categoria>();
+            CategoriasDisponiveis.Add(new Categoria("Camisas"));
+            CategoriasDisponiveis.Add(new Categoria("Calças"));
+            CategoriasDisponiveis.Add(new Categoria("Sapatos"));
+        }
+    }
+}
