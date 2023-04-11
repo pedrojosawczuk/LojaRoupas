46a630cacf5d7d1171aa64bab383324096b11a44
JonathanMCM <jmarinho@alunos.utfpr.edu.br>
2023-04-11 15:07:08 -0300

    Cliente

---
+++ SistemaGerenciamentoVendasLojaRoupas/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciamentoVendasLojaRoupas
+{
+	public class Cliente
+	{
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+
+        public Cliente(string nome, string sobrenome, string endereco, string telefone)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Endereco = endereco;
+            Telefone = telefone;
+        }
+
+    }
+
+
+}
