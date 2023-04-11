aa29625410d85d344558d4fc89ba02ec36a60326
Pedro J O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2023-04-11 14:41:40 -0300

    change namespace

--- SistemaGerenciamentoVendasLojaRoupas/Produto.cs
+++ SistemaGerenciamentoVendasLojaRoupas/Produto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GerenciamentoVendasLojaRoupas
+namespace SistemaGerenciamentoVendasLojaRoupas
 {
     internal class Produto
     {
