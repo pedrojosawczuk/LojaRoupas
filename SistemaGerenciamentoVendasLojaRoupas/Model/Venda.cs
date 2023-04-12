0a9dd85f50525b53fd9a0c150324b11033b43dde
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2023-04-11 23:23:45 -0300

    update: Venda.cs

--- SistemaGerenciamentoVendasLojaRoupas/Model/Venda.cs
+++ SistemaGerenciamentoVendasLojaRoupas/Model/Venda.cs
@@ -2,19 +2,43 @@ namespace SistemaGerenciamentoVendasLojaRoupas.Model
 {
     public class Venda
     {
-        public int id { get; set; }
-        private Cliente cliente { get; set; }
-        private List<Produto> produtos { get; set; }
-        private DateTime data { get; set; }
-        private double valorTotal { get; set; }
+        public int Id { get; set; }
+        private List<Cliente> _cliente;
+        private List<Produto> _produtos;
+        private DateTime _data;
+        private double _valorTotal;
 
-        public Venda(int id, Cliente cliente, List<Produto> produtos, DateTime data, double valorTotal)
+        public Venda(int id, List<Cliente> cliente, List<Produto> produtos, DateTime data, double valorTotal)
         {
-            this.id = id;
-            this.cliente = cliente;
-            this.produtos = produtos;
-            this.data = data;
-            this.valorTotal = valorTotal;
+            Id = id;
+            _cliente = cliente;
+            _produtos = produtos;
+            _data = data;
+            _valorTotal = valorTotal;
+        }
+
+        public List<Cliente> Cliente
+        {
+            get { return _cliente; }
+            set { _cliente = value; }
+        }
+
+        public List<Produto> Produtos
+        {
+            get { return _produtos; }
+            set { _produtos = value; }
+        }
+
+        public DateTime Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+        public double ValorTotal
+        {
+            get { return _valorTotal; }
+            set { _valorTotal = value; }
         }
     }
 }
